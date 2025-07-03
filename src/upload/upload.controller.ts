1636d6d5ebee1ef970543de59c3fde5e8d4ee16f
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2025-07-03 10:38:12 -0300

    Add file upload functionality with multer; update UploadController to handle file uploads and create .gitkeep in uploads directory

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -7,6 +7,10 @@ import {
   Param,
   Delete,
 } from '@nestjs/common';
+import { UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { UploadService } from './upload.service';
 import { CreateUploadDto } from './dto/create-upload.dto';
 import { UpdateUploadDto } from './dto/update-upload.dto';
@@ -16,10 +20,27 @@ export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
   @Post()
-  create(@Body() createUploadDto: CreateUploadDto) {
-    return this.uploadService.create(createUploadDto);
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads', // pasta onde os arquivos serão salvos
+        filename: (req, file, callback) => {
+          const uniqueSuffix =
+            Date.now() + '-' + Math.round(Math.random() * 1e9);
+          const ext = extname(file.originalname);
+          callback(null, `${file.fieldname}-${uniqueSuffix}${ext}`);
+        },
+      }),
+      limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    return {
+      message: 'Arquivo enviado com sucesso!',
+      filename: file.filename,
+      originalname: file.originalname,
+    };
   }
-
   @Get()
   findAll() {
     return this.uploadService.findAll();
